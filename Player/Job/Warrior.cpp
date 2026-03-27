8b1ed139e5482ef4b7dead72f7c7689dc0c90cbe
ywkim212 <ywkim212@gmail.com>
2026-03-27 13:23:50 +0900

    Add files via upload

--- Player/Job/Warrior.cpp
+++ Player/Job/Warrior.cpp
@@ -1,38 +1,20 @@
-#include "warrior.h"
-#include "../Monster/monster.h"
-#include <iostream>
-using namespace std;
+#include "Warrior.h"
+#include "Player.h"
 
-Warrior::Warrior(const string& nickname) : Player(nickname) {
-    job_name = "전사";
-    cout << "* 전사로 전직하였습니다." << '\n';
-	HP = 100;
-	MP = 30;
-	power = 30;
-	defence = 25;
-	accuracy = 10;
-	speed = 10;
+Warrior::Warrior(string nickname) : Player(nickname)
+{
+	jobname = nickname;
+	maxHp = 120;
+	curHp = 120;
+	atk = 3;
+	def = 20;
+	dodge = 5;
 }
 
-void Warrior::attack() {
-    cout << "* 장검을 휘두른다" << '\n';
-}
+void Warrior::Attack(Monster& monster)
+{
+	int damage = calculateDamage(atk, monster.GetDef());
+	if (damage < 0) damage = 0;
 
-void Warrior::attack(Monster* monster) {
-	int damage = power - monster->getDefence();
-	int DNChance = rand() % 100;
-	if (DNChance > 80) {
-		damage = power;
-		cout << "* 방어력 무시! ";
-	}
-	if (damage <= 0) damage = 1;
-	cout << "* " << monster->getName() << "에게 " << damage << "의 데미지를 입혔습니다." << '\n';
-	int newHP = monster->getHP() - damage;
-	monster->setHP(newHP);
-	if (newHP > 0) {
-		cout << "* " << monster->getName() << "의 남은 HP: " << newHP << '\n';
-	}
-	else {
-		cout << "* " << monster->getName() << "이(가) 쓰러졌습니다." << '\n';
-	}
+	monster.TakeDamage(damage, true);
 }
