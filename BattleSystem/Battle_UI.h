4f6c0d36b3e5621fb2b80f9cce45c32857745e23
khalicika-dev <khalicika@gmail.com>
2026-03-29 03:32:39 +0900

    Squashed commit of the following:
    commit cfd06634e0beb96730bc6bdda6f4d32208adcc61
    Author: khalicika-dev <khalicika@gmail.com>
    Date:   Sun Mar 29 03:30:24 2026 +0900
    
        design BattleUI
    
        BattleSystem과 Battle_UI의 코드 개선. 입력 예외처리 해결. 방어 및 인벤토리 사용은 추후 상의예정.

--- BattleSystem/Battle_UI.h
+++ BattleSystem/Battle_UI.h
@@ -2,6 +2,20 @@
 #include <string>
 #include "../Player/Player/Player.h"
 #include "../Player/Monster/Monster.h"
+#include "../System/Utility.h"
+#include "../System/Windows.h"
 using namespace std;
    
-void displayBattle(class Player& const p, class Monster& const m, string battleMsg = "", string actionMsg = "계속하려면 아무키를 눌러주세요.");
+struct BattleLog
+{
+	string line_1 = "";
+	string line_2 = "";
+	string line_3 = "";
+	void Clear()
+	{
+		line_1 = "";
+		line_2 = "";
+		line_3 = "";
+	}
+};
+void displayBattle(class Player& const p, class Monster& const m, Vector2D& curPos, BattleLog log, string actionMsg = "계속하려면 아무 키를 눌러주세요...");
--- Launch.cpp
+++ Launch.cpp
@@ -1,6 +1,10 @@
-#include <iostream>
+﻿#include <iostream>
 #include <string>
 #include <limits>
+
+#include "Player/Job/Thief.h"
+#include "Player/Monster/Monster.h"
+#include "BattleSystem/BattleSystem.h"
 
 using namespace std;
 
@@ -51,29 +55,34 @@ int getUserChoice() {
 void startGame() {
     cout << "\n게임을 시작합니다..." << '\n';
     //Todo : Game Start (캐릭터 생성)
+    Thief* p = new Thief("sans");
+    Monster* m = new Monster("aaa",1,100,5,3,1,1,1,1,1);
+
+    BattleSystem::getInstance().Battle(*p, *m);
+    
 }
 
-//int main() {
-//    bool isRunning = true;
-//    
-//    while (isRunning) {
-//        displayMenu();
-//        int choice = getUserChoice();
-//
-//        switch (choice) {
-//            case 1:
-//                startGame();
-//                break;
-//            case 2:
-//                cout << "\n게임을 종료합니다. 감사합니다!" << endl;
-//                isRunning = false;
-//                break;
-//            default:
-//                cout << "\n잘못된 입력입니다. 1 또는 2를 입력해주세요." << endl;
-//                break;
-//        }
-//    }
-//
-//    return 0;
-//}
+int main() {
+    bool isRunning = true;
+    
+    while (isRunning) {
+        displayMenu();
+        int choice = getUserChoice();
+
+        switch (choice) {
+            case 1:
+                startGame();
+                break;
+            case 2:
+                cout << "\n게임을 종료합니다. 감사합니다!" << endl;
+                isRunning = false;
+                break;
+            default:
+                cout << "\n잘못된 입력입니다. 1 또는 2를 입력해주세요." << endl;
+                break;
+        }
+    }
+
+    return 0;
+}
 
