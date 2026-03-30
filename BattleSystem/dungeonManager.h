1463b572f02774d44049f3f13d911cc02e52821f
bowonShin <bowo9700@Gamil.com>
2026-03-30 16:15:08 +0900

    몬스터 battle 및 delete 관리

--- BattleSystem/dungeonManager.h
+++ BattleSystem/dungeonManager.h
@@ -6,6 +6,7 @@
 #include "BattleSystem.h"
 #include "../Item/ItemDB.h"
 #include "../Shop/shopsystem.h"
+#include "../Player/Monster/MonsterDB.h"
 
 class Player;
 
@@ -39,7 +40,7 @@ public:
 private:
 	
 
-	static const int Last_Stage = 10; // 보스 등장 stage
+	static const int Last_Stage = 9; // 보스 등장 stage
 	static const int Shop_Stage = 3; // 3의 배율로 상점 등장
 	int BattleCount; //BattleCount 현제 플레이어이의 Stage
 	bool b_LifeCheck; // 플레이어 생존 유무 확인
@@ -48,15 +49,8 @@ private:
 	Loot tempData;
 	
 	ShopSystem Shop_;
-    /*
-    static dungeonManager* GetInstnace() // 싱글톤화
-    {
-        if (Instance == nullptr) // Instance 가 없으면
-            Instance = new dungeonManager*;  // 새로 할당
-        return Instance;// 할당되어있는 값 return ( 어차피 호출할 떄마다 투과됨)
-    }
-    */
-
+	MonsterGenerator Mons_g;
+	Monster* Monster_;
 
 public:
     dungeonManager()
@@ -68,7 +62,7 @@ public:
 
 	void Initialize(); // 시작 초기화
 	bool setMonster(vector<Monster*> mons);
-	void StartDungeon(Player* Player_,vector<Monster*> Mons);// player 데이터와 monster 데이터 받아서 실행 ( 몬스터 여러마리 넣을려먼 vector로 입력 받기 >
+	void StartDungeon(Player* Player_);// player 데이터와 monster 데이터 받아서 실행 ( 몬스터 여러마리 넣을려먼 vector로 입력 받기 >
 
 	bool HiddenRand(); // 히든던전 등장 확률
 	void EnterShop(Player* player_); // 상점 입장유무 
